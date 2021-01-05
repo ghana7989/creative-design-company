ad3eaf6e3fb0445860ec193386c6d4415fce115e
ghana <ghana.chindukuri@gmail.com>
2021-01-05 18:25:26 +0530

    Hero Section

---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log('window.screenX: ', window.screenX);
+// console.log('window.pageXOffset: ', window.innerWidth);
+if (Number(window.innerWidth) < 810) {
+  alert("Not yet built for this size of screen")
+  window.history.back()
+}
