9809fe29be280d0cb0a18d038e6de59518919d6f
ghana <ghana.chindukuri@gmail.com>
2021-01-06 18:25:38 +0530

    finished project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// console.log('window.screenX: ', window.screenX);
-// console.log('window.pageXOffset: ', window.innerWidth);
-if (Number(window.innerWidth) < 810) {
-  alert("Not yet built for this size of screen")
-  window.history.back()
-}
+document.querySelectorAll('.story-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('change')
+    btn.nextElementSibling.classList.toggle('change')
+  })
+})
